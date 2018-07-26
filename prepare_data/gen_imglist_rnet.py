6e466af548fe62cd4254995af0c34c3a9f83e944
yinyuecheng1 <yinyuecheng@mail.dlut.edu.cn>
2018-07-26 16:14:31 +0800

    modity

--- prepare_data/gen_imglist_rnet.py
+++ prepare_data/gen_imglist_rnet.py
@@ -23,23 +23,24 @@ with open(os.path.join(data_dir, '%s/neg_%s.txt' % (size, size)), 'r') as f:
 with open(os.path.join(data_dir, '%s/part_%s.txt' % (size, size)), 'r') as f:
     part = f.readlines()
 
+'''
 with open(os.path.join(data_dir, '%s/landmark_%s_aug.txt' % (size, size)), 'r') as f:
     landmark = f.readlines()
+'''
   
 dir_path = os.path.join(data_dir, 'imglists',"RNet")
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
 #write all data
 with open(os.path.join(dir_path, "train_%s_landmark.txt" % (net)), "w") as f:
-    print len(neg)
-    print len(pos)
-    print len(part)
-    print len(landmark)
+    print(len(neg))
+    print(len(pos))
+    print(len(part))
+    #print(len(landmark))
     for i in np.arange(len(pos)):
         f.write(pos[i])
     for i in np.arange(len(neg)):
         f.write(neg[i])
     for i in np.arange(len(part)):
         f.write(part[i])
-    for i in np.arange(len(landmark)):
-        f.write(landmark[i])
+
